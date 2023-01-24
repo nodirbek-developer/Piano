74a9da2debd040bd64a54e04fa8a13311ab19f63
nodirbek-developer <nodirbeknurbekov6@gmial.com>
2023-01-24 17:26:14 +0500

    pianino

---
+++ app.js
@@ -0,0 +1,18 @@
+const key=document.querySelectorAll(".key")
+function playNota(e) {
+    const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key=document.querySelector(`.key[data-key="${e.keyCode}"]`)
+
+    if(!key) return;
+
+    key.classList.add("playing");
+    audio.currentTime = 0;
+    audio.play();
+}
+function removeTransiton(e){
+    if(e.propertyName !== "transform") return;
+    this.classList.remove("playing");
+}
+
+key.forEach(key => key.addEventListener("transitionend", removeTransiton))
+window.addEventListener("keydown", playNota)
